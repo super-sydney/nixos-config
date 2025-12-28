a9e70c7e71eab444aa2774f1a5432a144d77c9b1
Sydney <sydneykho@proton.me>
2025-12-29 00:39:19 +0100

    Add vscode extensions and profiles

--- modules/editors/vscode.nix
+++ modules/editors/vscode.nix
@@ -8,7 +8,76 @@
   };
 
   config = lib.mkIf config.vscode.enable {
-    # Install VSCode through Home Manager
-    home.packages = with pkgs; [ vscode ];
+    programs.vscode = {
+      enable = true;
+
+      profiles =
+        let
+          defaultExtensions = with pkgs.vscode-extensions; [
+            catppuccin.catppuccin-vsc
+            catppuccin.catppuccin-vsc-icons
+            github.copilot-chat
+            esbenp.prettier-vscode
+            redhat.vscode-yaml
+            ms-vsliveshare.vsliveshare
+            yzhang.markdown-all-in-one
+            shd101wyy.markdown-preview-enhanced
+            usernamehw.errorlens
+          ];
+        in {
+          default = {
+            extensions = defaultExtensions;
+            userSettings = {
+              "editor.formatOnSave" = true;
+              "files.trimTrailingWhitespace" = true;
+              "files.insertFinalNewline" = true;
+            };
+          };
+
+          python = {
+            extensions = defaultExtensions ++ (with pkgs.vscode-extensions; [
+              ms-python.python
+              ms-python.vscode-pylance
+            ]);
+
+            userSettings = {
+              # "python.formatting.provider" = "black";
+              # "python.linting.enabled" = true;
+              # "python.linting.flake8Enabled" = true;
+              # "python.linting.mypyEnabled" = true;
+            };
+          };
+
+          javascript = {
+            extensions = defaultExtensions ++ (with pkgs.vscode-extensions; [
+              dbaeumer.vscode-eslint
+            ]);
+            userSettings = {
+            };
+          };
+
+          # C / C++ development profile
+          c-cpp = {
+            extensions = defaultExtensions ++ (with pkgs.vscode-extensions; [
+              ms-vscode.cpptools
+              ms-vscode.cmake-tools
+              twxs.cmake
+              ms-vscode.makefile-tools
+            ]);
+            userSettings = {
+            };
+          };
+
+          # Nix development profile
+          nix = {
+            extensions = defaultExtensions ++ (with pkgs.vscode-extensions; [
+              jnoortheen.nix-ide
+              arrterian.nix-env-selector
+            ]);
+            userSettings = {
+            };
+          };
+        };
+    };
   };
 }

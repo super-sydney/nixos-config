886388b4d9410955c91bc11a2bb487916a5846f7
Sydney <sydneykho@proton.me>
2025-12-30 21:29:45 +0100

    Add proton GE, gamescope and mangohud

--- modules/nixos/steam.nix
+++ modules/nixos/steam.nix
@@ -14,5 +14,16 @@
 
   config = lib.mkIf config.steam.enable {
     programs.steam.enable = true;
+    programs.steam.extraCompatPackages = with pkgs; [
+      proton-ge-bin
+    ];
+
+    programs.steam.gamescopeSession.enable = true;
+
+    environment.systemPackages = with pkgs; [
+      mangohud
+    ];
+
+    programs.gamemode.enable = true;
   };
 }

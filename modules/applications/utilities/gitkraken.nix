2dc9d20b1540cf251b4245baeb2fb57c902324b7
Sydney <sydneykho@proton.me>
2026-01-04 14:24:23 +0100

    Add gitkraken theme

--- modules/applications/utilities/gitkraken.nix
+++ modules/applications/utilities/gitkraken.nix
@@ -14,5 +14,12 @@
 
   config = lib.mkIf config.gitkraken.enable {
     home.packages = with pkgs; [ gitkraken ];
+
+    # Download and install Catppuccin Mocha theme
+    home.file.".gitkraken/themes/catppuccin-mocha.jsonc".source =
+      pkgs.fetchurl {
+        url = "https://raw.githubusercontent.com/catppuccin/gitkraken/main/themes/catppuccin-mocha.jsonc";
+        sha256 = "sha256-u97bjJi3V2AI8Hw9wI25KfSe4bneX0QcOU0rzmeGaMM=";
+      };
   };
 }

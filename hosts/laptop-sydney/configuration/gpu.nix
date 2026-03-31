d73e8d75308f1edca336f3e1330bca0fb803387f
Sydney <sydneykho@proton.me>
2026-03-31 16:26:15 +0200

    Use open kernel modules (recommended by nvidia)

--- hosts/laptop-sydney/configuration/gpu.nix
+++ hosts/laptop-sydney/configuration/gpu.nix
@@ -58,8 +58,7 @@
       services.xserver.videoDrivers = [ "nvidia" ];
 
       hardware.nvidia = {
-        package = config.boot.kernelPackages.nvidiaPackages.beta;
-        open = false;
+        open = true;
         modesetting.enable = true;
         powerManagement.enable = true;
         prime = {
